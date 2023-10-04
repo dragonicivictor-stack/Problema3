c3a904700973baf9a013efef475fa6c32e10e860
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-04 21:29:52 +0300

    solutie pentru problema 3

---
+++ Problema3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Problema3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti sirul de caractere: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Introduceti cuvantul cautat: ");
+            string subtext = Console.ReadLine();
+
+            int aparatii = 0;
+            
+            for (int i = 0; i <= text.Length - subtext.Length; i++)
+            {
+                if (text.Substring(i, subtext.Length).Equals(subtext, StringComparison.OrdinalIgnoreCase))
+                {
+                    aparatii++;
+                }
+            }
+
+            Console.WriteLine("Cuvantul cautat apare de " + aparatii + " ori");
+
+        }
+    }
+}
